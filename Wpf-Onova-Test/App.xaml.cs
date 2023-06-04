31cf3e11636c38cf8beafe28bfb406fda502f6ef
diberium <doylecnn@gmail.com>
2023-06-04 11:02:51 +0800

    添加项目文件。

---
+++ Wpf-Onova-Test/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Onova;
+using Onova.Services;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf_Onova_Test {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App: Application {
+        protected override async void OnStartup(StartupEventArgs e) {
+            await CheckUpdate();
+        }
+
+        private async Task CheckUpdate() {
+            using IUpdateManager _updateManager = new UpdateManager(
+                new LocalPackageResolver(@"c:\temp\wpf-onova-test", "Wpf-Onova-Test-v*.zip"),
+                new ZipPackageExtractor());
+
+            var check = await _updateManager.CheckForUpdatesAsync();
+
+            if (check.CanUpdate) {
+                await _updateManager.PrepareUpdateAsync(check!.LastVersion!);
+                MessageBox.Show("app will be update and restart.");
+
+                _updateManager.LaunchUpdater(check!.LastVersion!, true);
+                this.Shutdown();
+            }
+        }
+    }
+}
